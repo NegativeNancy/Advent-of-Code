c8814b2941b99a123ce845887d9a18d3f690e7e7
Ivo H <23632852+NegativeNancy@users.noreply.github.com>
2021-12-02 14:24:05 +0100

    Update day 2 - added solution 2

--- 2021/day-2.py
+++ 2021/day-2.py
@@ -10,6 +10,13 @@ def get_postion(data):
     return movement, int(steps)
     
 
+def calculate_aim(movement, step, aim):
+    if movement == 'up':
+        return (aim - step)
+    else:
+        return (aim + step)
+
+
 def solution1(data):
     data_l = len(data)
     c, horizontal, depth = 0, 0, 0
@@ -20,17 +27,29 @@ def solution1(data):
             horizontal, c = (horizontal + steps), (c + 1)
         elif (movement == 'down'):
             depth, c = (depth + steps), (c + 1)
-        elif (movement == 'up'):
-            depth, c = (depth - steps), (c + 1)
         else:
-            print("Invalid movement detected")
+            depth, c = (depth - steps), (c + 1)
 
     final_calc = horizontal * depth
     print("Solution 1 caluclation: ", final_calc)
 
 def solution2(data):
-    pass
+    data_l = len(data)
+    c, horizontal, depth, aim = 0, 0, 0, 0
 
+    while c < data_l:
+        movement, steps = get_postion(data[c])
+        if (movement == 'down'):
+            aim, c = calculate_aim(movement, steps, aim), (c + 1)
+        elif (movement == 'up'):            
+            aim, c = calculate_aim(movement, steps, aim), (c + 1)
+        else: 
+            horizontal, c = (horizontal + steps), (c + 1)
+            depth = depth + (steps * aim)
+        
+    final_calc = horizontal * depth
+    print("Solution 2 caluclation: ", final_calc)
+            
 
 def main():
     # data = get_input("test-data.txt")
